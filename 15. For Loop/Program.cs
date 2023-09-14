253371d7750753c164f88240b62d1a076880c82e
user <user@LAPTOP-KAN0NU4I>
2023-09-15 00:12:31 +0200

    Add project files.

---
+++ 15. For Loop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.For_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the for Loop");
+
+            for(int i=0; i<=5; i++) 
+            { 
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("This is the WHILE Loop");
+            int j = 0;
+
+            while (j <= 5) 
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+        }
+    }
+}
